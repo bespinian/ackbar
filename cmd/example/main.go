780c53bba9dcf2e991311a3f96790372c4342031
Mathis Kretz <mkretz@users.noreply.github.com>
2023-11-05 23:04:19 +0100

    Add a simple example application

---
+++ cmd/example/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-http-utils/headers"
+)
+
+func main() {
+
+	landoUrl := os.Getenv("LANDO_URL")
+	contextId := os.Getenv("CONTEXT_ID")
+
+	log.Printf("Starting worker in context %s", contextId)
+
+	workersUrl := fmt.Sprintf("%s/contexts/%s/workers", landoUrl, contextId)
+
+	log.Printf("Registering worker at %s", workersUrl)
+	res, err := http.Post(workersUrl, "Content-Type:application/json", strings.NewReader(""))
+	if err != nil {
+		log.Fatalf("Failed registering worker: %e", err)
+	}
+
+	workerUrl := fmt.Sprintf("%s%s", landoUrl, res.Header.Get(headers.Location))
+	log.Printf("Refresh URL is %s", workerUrl)
+
+	log.Printf("Starting processing")
+	defer res.Body.Close()
+
+	for {
+		time.Sleep(5 * time.Second)
+		log.Printf("refreshing %s", workerUrl)
+		req, err := http.NewRequest(http.MethodPut, workerUrl, nil)
+		if err != nil {
+			log.Fatalf("Failed creating refresh request: %e", err)
+		}
+		_, err = http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatalf("Failed to refresh worker: %e", err)
+		}
+	}
+
+}
